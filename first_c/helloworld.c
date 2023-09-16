e9c2a7262465a266093e41b2c400ba822b2dd1ba
Abdullah Naim <aanaim96@gmail.com>
2023-09-16 15:28:39 -0400

    Add files via upload

---
+++ first_c/helloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char characterName[] = "Abdullah";
+    int characterAge = 27;
+    printf("Hello my name is %s!\n", characterName);
+    printf("%d is going to be the programming king!\n", characterAge);
+    return 0;
+}
